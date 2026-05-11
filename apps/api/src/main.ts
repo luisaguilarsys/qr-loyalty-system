1540028459b7224f51fa7cb881048541217b27f9
luisaguilarsys <84945882+luisaguilarsys@users.noreply.github.com>
2026-05-10 20:20:00 -0600

    feat(docker): add api service

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,13 @@
-import 'dotenv/config'; // this is required to load .env file in local workspace without docker
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  if (process.env.NODE_ENV != 'production') {
+    await import('dotenv/config');
+  }
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().then(() =>
+  console.log(`Server: http://localhost:${process.env.PORT ?? 3000}`),
+);

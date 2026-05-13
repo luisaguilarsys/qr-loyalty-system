fb0559062cae8aa9a6c3b19b151cf2cf7e3c1cde
luisaguilarsys <84945882+luisaguilarsys@users.noreply.github.com>
2026-05-13 11:15:00 -0600

    refactor(api): bootstrap response

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { PORT = 4000 } = process.env;
+const { IP_HOST = 'localhost' } = process.env;
+
 async function bootstrap() {
   if (process.env.NODE_ENV != 'production') {
     await import('dotenv/config');
@@ -8,6 +11,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap().then(() =>
-  console.log(`Server: http://localhost:${process.env.PORT ?? 3000}`),
-);
+
+bootstrap()
+  .then(() => console.log(`[API]: Server: http://${IP_HOST}:${PORT}`))
+  .catch((err) => {
+    console.error('[API]: Error: Unable to initialize server');
+    console.error(err);
+  });

388a38169f05ab1d27fc5d1291d1debd121d14c4
luisaguilarsys <84945882+luisaguilarsys@users.noreply.github.com>
2026-06-23 19:27:43 -0600

    feat(api): initialize new customers with a 0 balance upon creation

--- apps/api/src/customers/customers.service.ts
+++ apps/api/src/customers/customers.service.ts
@@ -10,146 +10,178 @@ import { QrService } from '@/common/qr/qr.service';
 
 @Injectable()
 export class CustomersService {
-  constructor(private readonly prismaService:PrismaService, private readonly encryptionService:EncryptionService, private readonly qrService:QrService){}
-
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly encryptionService: EncryptionService,
+    private readonly qrService: QrService,
+  ) {}
 
   async create(createCustomerDto: CreateCustomerDto) {
+    const { name, middleName, lastName, email, phone, birthDate } =
+      createCustomerDto;
 
-    const { name, middleName, lastName, email, phone, birthDate } = createCustomerDto
-
-    const data  = {
+    const data = {
       name,
       middleName,
       lastName,
-      email:this.encryptionService.encrypt(email),
+      email: this.encryptionService.encrypt(email),
       emailHash: this.encryptionService.hash(email),
-      phone:this.encryptionService.encryptOptional(phone),
+      phone: this.encryptionService.encryptOptional(phone),
       birthDate,
-    }
+    };
 
-    const customerCreated = await this.prismaService.customer.create({data})
-    const customerQr = await this.prismaService.customerQr.create({data:{customerId:customerCreated.id,reason:"new member"}})
+    const customerCreated = await this.prismaService.$transaction(
+      async (tx) => {
+        const customer = await tx.customer.create({ data });
+
+        await tx.customerQr.create({
+          data: {
+            customerId: customer.id,
+            reason: 'new member',
+          },
+        });
+
+        await tx.customerBalance.create({
+          data: {
+            customerId: customer.id,
+            points: 0,
+          },
+        });
+
+        return customer;
+      },
+    );
 
     return customerCreated;
   }
 
-  private buildWhere(query:FindCustomersQueryDto):Prisma.CustomerWhereInput{
-    const {name,email, memberNumber, qrToken, isActive=true} = query
-    const where: Prisma.CustomerWhereInput = { isActive}
+  private buildWhere(query: FindCustomersQueryDto): Prisma.CustomerWhereInput {
+    const { name, email, memberNumber, qrToken, isActive = true } = query;
+    const where: Prisma.CustomerWhereInput = { isActive };
 
-    if(name){
+    if (name) {
       where.OR = [
-        {name: {contains:name,mode:'insensitive'}},
-        {middleName:{contains:name,mode:'insensitive'}},
-        {lastName:{contains:name,mode:'insensitive'}}
-      ]
+        { name: { contains: name, mode: 'insensitive' } },
+        { middleName: { contains: name, mode: 'insensitive' } },
+        { lastName: { contains: name, mode: 'insensitive' } },
+      ];
     }
 
-    if(email){
-      where.emailHash = this.encryptionService.hash(email)
+    if (email) {
+      where.emailHash = this.encryptionService.hash(email);
     }
 
-    if(memberNumber){
-      where.memberNumber = memberNumber
+    if (memberNumber) {
+      where.memberNumber = memberNumber;
     }
 
-    if(qrToken){
-      where.qrCodes={some:{token:qrToken,isActive:true}}
+    if (qrToken) {
+      where.qrCodes = { some: { token: qrToken, isActive: true } };
     }
 
-    return where
-
+    return where;
   }
 
-  async findAll(query:FindCustomersQueryDto):Promise<Customer[]> {
-    const {page=1, limit=20} = query
-    const skip = (page-1) * limit
-    const where = this.buildWhere(query)
-    
+  async findAll(query: FindCustomersQueryDto): Promise<Customer[]> {
+    const { page = 1, limit = 20 } = query;
+    const skip = (page - 1) * limit;
+    const where = this.buildWhere(query);
 
-
-    const [customers,total] =await Promise.all([
+    const [customers, total] = await Promise.all([
       this.prismaService.customer.findMany({
         where,
         skip,
-        take:limit,
-        orderBy:{createdAt:'desc'},
-        include:{balance:true},
+        take: limit,
+        orderBy: { createdAt: 'desc' },
+        include: { balance: true },
       }),
-      this.prismaService.customer.count({where})
-    ])
+      this.prismaService.customer.count({ where }),
+    ]);
 
     return customers;
   }
 
-  async findOneById(id: UUID):Promise<Customer> {
-    const customer = await this.prismaService.customer.findUnique({where:{id}})
-    if(!customer) throw new NotFoundException(`Customer with id ${id} not found`)
+  async findOneById(id: UUID): Promise<Customer> {
+    const customer = await this.prismaService.customer.findUnique({
+      where: { id },
+    });
+    if (!customer)
+      throw new NotFoundException(`Customer with id ${id} not found`);
     return customer;
   }
 
-  async findByEmail(email:string):Promise<Customer>{
+  async findByEmail(email: string): Promise<Customer> {
     const customer = await this.prismaService.customer.findUnique({
-      where:{emailHash:this.encryptionService.hash(email)}
-    })
-    if(!customer) throw new NotFoundException(`Customer with email) ${email} not found`) 
+      where: { emailHash: this.encryptionService.hash(email) },
+    });
+    if (!customer)
+      throw new NotFoundException(`Customer with email) ${email} not found`);
 
-    return customer
+    return customer;
   }
 
-  async findByName(name:string):Promise<Customer[]>{
+  async findByName(name: string): Promise<Customer[]> {
     const customers = await this.prismaService.customer.findMany({
-      where:{
-        isActive:true,
-        OR:[
-          {name:{contains:name,mode:'insensitive'}},
-          {middleName:{contains:name,mode:"insensitive"}},
-          {lastName:{contains:name,mode:'insensitive'}},
-        ]
+      where: {
+        isActive: true,
+        OR: [
+          { name: { contains: name, mode: 'insensitive' } },
+          { middleName: { contains: name, mode: 'insensitive' } },
+          { lastName: { contains: name, mode: 'insensitive' } },
+        ],
       },
-      include:{balance:true}
-    })
+      include: { balance: true },
+    });
 
-    return customers
+    return customers;
   }
-
-
 
   async update(id: UUID, updateCustomerDto: UpdateCustomerDto) {
-    const customerUpdated = await this.prismaService.customer.update({where:{id},data:updateCustomerDto})
+    const customerUpdated = await this.prismaService.customer.update({
+      where: { id },
+      data: updateCustomerDto,
+    });
 
     return customerUpdated;
   }
 
   async remove(id: UUID) {
-    const customerDeleted = await this.prismaService.customer.update({where:{id},data:{isActive:false}})
-    if(!customerDeleted) throw new NotFoundException(`Customer with id ${id} not found`)
+    const customerDeleted = await this.prismaService.customer.update({
+      where: { id },
+      data: { isActive: false },
+    });
+    if (!customerDeleted)
+      throw new NotFoundException(`Customer with id ${id} not found`);
     return;
   }
 
-
   // Qr Code generation for customers
 
-  async getQrImage(customerId:string): Promise<Buffer>{
-    const qrRecord = await this.prismaService.customerQr.findFirst({where:{customerId,isActive:true}})
+  async getQrImage(customerId: string): Promise<Buffer> {
+    const qrRecord = await this.prismaService.customerQr.findFirst({
+      where: { customerId, isActive: true },
+    });
 
-    if(!qrRecord) throw new NotFoundException('Qr record not found')
-    
-    return this.qrService.generateQrBuffer({v:1,token:qrRecord.token})
+    if (!qrRecord) throw new NotFoundException('Qr record not found');
 
+    return this.qrService.generateQrBuffer({ v: 1, token: qrRecord.token });
   }
 
   // Re-generate a new Qr Code for customer
 
-  async regenerateQr(customerId:string,reason?:string){
-    await this.prismaService.customerQr.updateMany({where:{customerId,isActive:true},data:{isActive:false,deactivatedAt:new Date()}})
-    
+  async regenerateQr(customerId: string, reason?: string) {
+    await this.prismaService.customerQr.updateMany({
+      where: { customerId, isActive: true },
+      data: { isActive: false, deactivatedAt: new Date() },
+    });
+
     const newCustomerQr = await this.prismaService.customerQr.create({
-      data:{customerId,reason}
-    })
+      data: { customerId, reason },
+    });
 
-    return this.qrService.generateQrBuffer({v:1,token:newCustomerQr.token})
+    return this.qrService.generateQrBuffer({
+      v: 1,
+      token: newCustomerQr.token,
+    });
   }
-
-
 }
